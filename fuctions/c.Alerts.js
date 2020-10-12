cc1306a065a7dafe2e3a6db876c7dfa898958e20
Mabulane <58472806+Mabulane@users.noreply.github.com>
2020-10-12 14:37:15 +0200

    geoLocatio

--- fuctions/c.Alerts.js
+++ fuctions/c.Alerts.js
@@ -3,13 +3,29 @@ const formCtAlert = document.querySelector('#create-alert');
 formCtAlert.addEventListener('submit',(e) =>{
     e.preventDefault();
     const posteddate =new Date();
+    
+//var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    //x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+var longi,lati;
+function showPosition(position) {
+ lati = position.coords.latitude;
+ longi=position.coords.longitude;
+}
+    
    db.collection('Alerts').add({
           name: formCtAlert['altName'].value,
           description: formCtAlert['disc'].value,
           location:formCtAlert['location'].value,
           //status:formCtAlert['location'].value,
           //reqMedAssistance:formCtAlert['location'].value,
-          //geolocation:formCtAlert['location'].value,
+          geolocation:[longi,lati],
           dayPosted:posteddate
    }).then(() =>{
     formCtAlert.reset();
